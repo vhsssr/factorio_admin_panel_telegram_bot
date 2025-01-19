874bd7a2934867f8578800c83bc8c4ef1de44556
vhs_s <vhs_ssr@outlook.com>
2025-01-19 22:03:00 +0100

    fix paths

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "installing bot service, make sure you set all needed paths"
-chmod +x install_bot_service.sh
-./install_bot_service.sh
+chmod +x ./bin/install_bot_service.sh
+sudo ./bin/install_bot_service.sh
 
 echo "installing factorio service"
-chmod +x install_factorio.sh
-sudo ./install_factorio.sh
+chmod +x ./bin/install_factorio.sh
+sudo ./bin/install_factorio.sh
